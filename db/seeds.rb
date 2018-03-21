d336eb71ce3f81c3141d4279c62453a25934e232
Pavel Borisov <pavel.borisov@gotechsoftware.com>
2018-03-21 17:40:23 +0300

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ name: 'Pavel' }, { name: 'Tester' }])
+thread = CommentThread.create()
+
+thread.comments << Comment.new({ text: "Top level comment", user: users[0] })
+thread.comments << Comment.new({ text: "Another Top level comment", user: users[0] })
+
+thread.comments[0].comments << Comment.new({ text: 'Answer to first one', user: users[1] })
+thread.comments[1].comments << Comment.new({ text: 'Answer to second one', user: users[1] })
+
+thread.comments[0].comments[0].comments << Comment.new({ text: 'Reply to first answer', user: users[0] })
